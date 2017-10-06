25ac47e9699b9a651fadc255e8d75b8c993eb0bb
unknown <fer0309g@gmail,com>
2017-10-05 19:31:00 -0700

    Finished addition project

--- Hw/js/addition.js
+++ Hw/js/addition.js
@@ -1,1 +1,10 @@
-// Addition 
+// Addition
+
+var form = document.querySelector("form");
+form.addEventListener("submit", function(e){
+  e.preventDefault();
+  var x =  new Number(e.target[0].value);
+  var y = new Number(e.target[1].value);
+  var z = x + y;
+  alert(z);
+})
