1f978eea3082dd8187728ce0edf9b54af763f2a2
unknown <fer0309g@gmail,com>
2017-10-05 20:38:34 -0700

    capitalization assignment finished

--- Hw/js/uppercase.js
+++ Hw/js/uppercase.js
@@ -1,1 +1,16 @@
 // Uppercase Your Name
+
+var form = document.querySelector("form");
+console.log(form);
+form.addEventListener("submit", function(e){
+  e.preventDefault();
+  var outcome = e.target[0].value;
+  alert(outcome.toUpperCase());
+});
+
+
+function myButton() {
+    document.getElementById("mySubmit").value = "CAPITALIZE!";
+};
+
+myButton();
